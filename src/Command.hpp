def07c08606ff6ef00ca6876661ddfa435e5c578
Felix Chen <racoperdb@gmail.com>
2026-01-08 17:24:13 +0800

    Table-driven

--- src/Command.hpp
+++ src/Command.hpp
@@ -1,48 +1,50 @@
 #pragma once
+#include <functional>
+
 #include "ExecutorImpl.hpp"
 #include "PoseHandler.hpp"
 namespace adas
 {
-class ICommand
+// class ICommand
+// {
+//     public : virtual ~ICommand() = default;
+//     virtual void DoOperate(PoseHandler & poseHandler) const noexcept = 0;
+// };
+class MoveCommand final  // : public ICommand
 {
-    public : virtual ~ICommand() = default;
-    virtual void DoOperate(PoseHandler & poseHandler) const noexcept = 0;
-};
-class MoveCommand final : public ICommand
-{
-    public : void DoOperate(PoseHandler& poseHandler) const noexcept override
-    {
+public:
+    const std::function<void(PoseHandler& poseHandler)> operate = [](PoseHandler& poseHandler) noexcept {
         if (poseHandler.IsFast()) {
             poseHandler.Move();
         }
         poseHandler.Move();
-    }
+    };
 };
-class TurnLeftCommand final : public ICommand
+class TurnLeftCommand final  // : public ICommand
 {
-    public : void DoOperate(PoseHandler& poseHandler) const noexcept override
-    {
+public:
+    const std::function<void(PoseHandler& poseHandler)> operate = [](PoseHandler& poseHandler) noexcept {
         if (poseHandler.IsFast()) {
             poseHandler.Move();
         }
         poseHandler.TurnLeft();
-    }
+    };
 };
-class TurnRightCommand final : public ICommand
+class TurnRightCommand final  // : public ICommand
 {
-    public : void DoOperate(PoseHandler& poseHandler) const noexcept override
-    {
+public:
+    const std::function<void(PoseHandler& poseHandler)> operate = [](PoseHandler& poseHandler) noexcept {
         if (poseHandler.IsFast()) {
             poseHandler.Move();
         }
         poseHandler.TurnRight();
-    }
+    };
 };
-class FastCommand final : public ICommand
+class FastCommand final  // : public ICommand
 {
-    public : void DoOperate(PoseHandler& poseHandler) const noexcept override
-    {
+public: 
+    const std::function<void(PoseHandler & poseHandler)> operate = [](PoseHandler& poseHandler) noexcept {
         poseHandler.Fast();
-    }
+    };
 };
 }  // namespace adas
